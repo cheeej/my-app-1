af31b9ea5e8be1b3463af87f822c1e3a4238fd51
eehd80 <eehd80yul@gmail.com>
2023-10-30 13:22:18 +0900

    components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Header(props) {
+    console.log(props); // const props = {title: "리액트(React)"}
+    return (
+        <header>
+            <h1>{props.title}</h1>
+            <Nav />
+        </header>
+    );
 }
 
+function Nav() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <a href="/">html</a>
+                </li>
+                <li>
+                    <a href="/">css</a>
+                </li>
+                <li>
+                    <a href="/">js</a>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+function Article() {
+    return (
+        <article>
+            <h2>타이틀1</h2>
+            <p>Hello, React</p>
+        </article>
+    );
+}
+
+function App() {
+    return (
+        <div className="root">
+            {/* sum(1, 2) */}
+            <Header title="리액트(React)" />
+            <Nav />
+            <Article />
+            <Article />
+            <Article />
+        </div>
+    );
+}
+
+// App을 기본으로 내보내기
 export default App;
---
+++ src/Module.jsx
@@ -0,0 +1,11 @@
+export default function A() {
+    return <div>a 출력</div>;
+}
+
+export function B() {
+    return <div>b 출력</div>;
+}
+
+export function C() {
+    return <div>c 출력</div>;
+}
