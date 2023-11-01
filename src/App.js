9c3938a9182d89ab86e0598ff4f1be86d27aefea
choi11223344 <choi890220@gmail.com>
2023-11-01 15:52:31 +0900

    props. map

--- src/App.js
+++ src/App.js
@@ -1,58 +1,57 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 function Header(props) {
-    console.log(props); // const props = {title: "리액트(React)"}
-    return (
-        <header>
-            <h1>{props.title}</h1>
-            <Nav />
-        </header>
-    );
+  console.log(props); // const props = {title: "리액트(React)"}
+  return (
+    <header>
+      <h1>{props.title}</h1>
+      {/* <Nav /> */}
+    </header>
+  );
 }
 
-function Nav() {
-    return (
-        <nav>
-            <ul>
-                <li>
-                    <a href="/">html</a>
-                </li>
-                <li>
-                    <a href="/">css</a>
-                </li>
-                <li>
-                    <a href="/">js</a>
-                </li>
-            </ul>
-        </nav>
-    );
+function Nav(props) {
+  // map() 함수를 사용한 배열 출력
+  const list = props.nav.map((item, index) => (
+    <li key={index}>
+      <a href={"/sub/" + item.title}>{item.title}</a>
+    </li>
+  ));
+
+  return (
+    <nav>
+      <ul>{list}</ul>
+    </nav>
+  );
 }
 
-function Article() {
-    return (
-        <article>
-            <h2>타이틀1</h2>
-            <p>Hello, React</p>
-        </article>
-    );
+function Article(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      <p>{props.desc}</p>
+    </article>
+  );
 }
 
 function App() {
-    return (
-        <div className="root">
-            {/* sum(1, 2) */}
-            <HeaderStyled title="리액트(React)" />
-            <Article />
-            <Article />
-            <Article />
-        </div>
-    );
+  const navArr = [{ title: "html" }, { title: "css" }, { title: "js" }];
+  return (
+    <div className="root">
+      {/* sum(1, 2) */}
+      <HeaderStyled title="리액트(React)" />
+      <Nav nav={navArr} />
+      <Article title="리액트란?" desc="desc1" />
+      <Article title="컴포넌트란?" desc="desc2" />
+      <Article title="props란?" desc="desc3" />
+    </div>
+  );
 }
 
 const HeaderStyled = styled(Header)`
-    display: flex;
-    border-bottom: 1px solid #eee;
+  display: flex;
+  border-bottom: 1px solid #eee;
 `;
 
 // App을 기본으로 내보내기
