8a08e12a3e69dbb041419fe72c58e4038db6956f
eehd80 <eehd80yul@gmail.com>
2023-11-01 10:59:52 +0900

    my-app

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import styled from 'styled-components';
+
 function Header(props) {
     console.log(props); // const props = {title: "리액트(React)"}
     return (
@@ -39,14 +42,18 @@ function App() {
     return (
         <div className="root">
             {/* sum(1, 2) */}
-            <Header title="리액트(React)" />
-            <Nav />
+            <HeaderStyled title="리액트(React)" />
             <Article />
             <Article />
             <Article />
         </div>
     );
 }
+
+const HeaderStyled = styled(Header)`
+    display: flex;
+    border-bottom: 1px solid #eee;
+`;
 
 // App을 기본으로 내보내기
 export default App;
